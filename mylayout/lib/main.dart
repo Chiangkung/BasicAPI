60826b8cee4f7029831eebd81e7680735c7996d0
Chiangkung <78609112+Chiangkung@users.noreply.github.com>
2021-09-04 20:11:50 +0700

    upload project layout ครั้งแรก

---
+++ mylayout/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mylayout/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Vaccine Knowledge",
+      home: HomePage(),
+    );
+  }
+}
+
